4b0cd329fbf5cb15e53f8211c69c4663fb63bbc6
Raphael Felício Domingues <raphaeldomingues84@gmail.com>
2022-03-19 19:04:14 -0300

    up exerc

---
+++ CURSO - JavaScript/aula014 - Exercícios/exercício 016/script.js
@@ -0,0 +1,9 @@
+function calcular(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (ini.value.length ==0 || fim.value.length ==0 || passo.value.length ==0 ||) {
+        window.alert('[ERRO] Faltam Dados!')
+    }
+}
