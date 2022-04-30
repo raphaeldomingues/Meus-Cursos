4e4c602970506a81d049706ea48c602976aa1515
Raphael Felício Domingues <raphaeldomingues84@gmail.com>
2022-04-30 12:57:40 +0100

    update JS

---
+++ CURSO - JavaScript/Mod. F - Funções/funcao02.js
@@ -0,0 +1,16 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(7, 3))
+// Resp. = 10
+
+// Na ausência de um parâmetro, o valor definido será 0
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(7))
+
+// Resp. = 7
